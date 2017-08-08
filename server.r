50467575b43aba36a6e027359f9e2f0489eb43e2
bendenis <bdshaff@umich.edu>
2017-08-08 14:27:38 -0400

    Restarting development.

--- server.r
+++ server.r
@@ -1,75 +1,83 @@
 library(shiny)
 library(dplyr)
 
-academicData = DT[,c("AverageAdmittedGPA","SATAverageComposite","SATAverageMath",
-                     "SATAverageReading","SATAverageWriting","SATRangeLower",
-                     "SATRangeUpper","ACTAverageEnglish","ACTAverageMath",
-                     "ACTAverageWriting","ACTAverageComposite",
-                     "ACTRangeLower","ACTRangeUpper",
-                     "GradLaw","GradMedical","GradDental","GradEngineering","GradTheology",
-                     "GradEducation","GradArtsSciences","GradVeterinary","GradMBA",
-                     "Specialized","SelectivityRank","SchoolName")]
-
-academicData = na.omit(academicData)
-
-academicDataMat = scale(academicData[,-25], center = TRUE, scale = TRUE)
-
-academicDistMat = as.matrix(dist(academicDataMat, method = "minkowski", p = 1))
-
-colnames(academicDistMat) = academicData$SchoolName
-row.names(academicDistMat) = academicData$SchoolName
-
-college_recommend <- function (user_data,weights){
-        for (i in 1:nrow(reco)) { 
-                cols = c('ACTRangeUpper','SATRangeUpper','AverageAdmittedGPA','volunteer_hour','award','ap_score','writing')
-                vec = user_data[1,cols]/reco[i,cols]
-                vec[1,is.na(vec[1,])] = 1
-                vec[1,(vec[1,4:7] > 1)] = 1
-                reco$admit_chance[i] = round(sum(vec*weights),3)
-        }
-        output = reco[,c('rank_overall','SchoolName','tuition_fee','location','admit_chance')]
-        # risk college
-        df = output %>%
-                filter(admit_chance<0.99) %>%
-                top_n(5,-rank_overall)
-        # fit college
-        df1 = output %>%
-                filter(admit_chance<=1.01 & admit_chance>=0.99) %>%
-                top_n(10,-rank_overall)
-        # safe college
-        df2 = output %>%
-                filter(admit_chance > 1.02 ) %>%
-                top_n(5,-rank_overall)
-        #df as final output for this user
-        df = rbind(df,df1,df2) %>%
-                arrange(admit_chance)
-        
-        return (df)
-}
-
 
 
 server <- function(input, output){
         
-        output$recommended_schools = renderDataTable({
-                
-              user_data = reco[192,]
-              user_data[1] = input$ACTscore
-              user_data[2] = input$SATscore
-              user_data[3] = input$GPA
-              user_data[9] = input$volunteer
-              user_data[10] = input$awards
-              user_data[11] = input$writing
-              user_data[12] = input$APscore
-              weights = c(0.25,0.25,0.1,0.1,0.1,0.1,0.1)
-              college_recommend(user_data, weights)
-                
-        })
+        output$institution_type_table = renderTable(
+                DT %>% filter(SchoolName == input$school_name) %>% select(PublicPrivate, FourYear, TwoYear, UgradTotalPop)
+        )
         
-        output$similar_school = renderDataTable({
-                lst = sort(academicDistMat[which(academicData$SchoolName == input$school_name),])[-1]
-                tb = data.frame(Name = names(lst))
-                tb
-        })
+        output$location_table = renderTable(
+                DT %>% filter(SchoolName == input$school_name) %>% select(State.x, City, Region, Longitude, Latitude, CampusType, LocationDescription)
+        )
+
+        output$ranking_table = renderTable(
+                DT %>% filter(SchoolName == input$school_name) %>% select(rank_overall, rank_art, rank_business, 
+                                                                          rank_cs, rank_medical, rank_engineer,
+                                                                          rank_life_science, rank_physical_science,
+                                                                          rank_social_science)
+        )
+        
+        output$admissions_table = renderTable(
+                DT %>% filter(SchoolName == input$school_name) %>% select(Acceptance.Rate, SATRangeUpper, SATRangeLower,
+                                                                          ACTRangeUpper, ACTRangeLower,
+                                                                          SATAverageComposite, ACTAverageComposite,
+                                                                          SATAverageReading, SATAverageMath, SATAverageWriting,
+                                                                          ACTAverageEnglish, ACTAverageMath, ACTAverageWriting,
+                                                                          ApplicationFee, Requirements)
+        )
+        
+        output$financial_table = renderTable(
+                DT %>% filter(SchoolName == input$school_name) %>% select(InstateTuition, OutstateTuition, 
+                                                                          SEOGScholarship, StateScholarship, CollegeScholarship,
+                                                                          PellGrantScholarship, NeedBasedScholarship, UnitedNegroScholarship,
+                                                                          NursingScholarship, PrivateScholarship, OtherScholarship,
+                                                                          PLUSLoan, StateLoan, CollegeLoan, NursingLoan, PerkinsLoan,
+                                                                          SubsudisedStaffordLoan, unSubsudisedStaffordLoan, OtherLoan)
+        )
+        
+        output$academics_table = renderTable(
+                DT %>% filter(SchoolName == input$school_name) %>% select(student_staff_ratio, subjects_offered) 
+        )
+        
+        
+        output$student_body_table = renderTable(
+                DT %>% filter(SchoolName == input$school_name) %>% select(UgradTotalPop, UgradMalePop, UgradFemalePop,
+                                                                          WhiteUgradPop, NativeUgradPop, ForeignUgradPop,
+                                                                          LatinUgradPop, BlackUgradPop, AsianUgradPop,
+                                                                          HawaiianUgradPop, MixraceUgradPop, NoRaceUgradPop,
+                                                                          WhiteFreshmanPop, NativeFreshmanPop, ForeignFreshmanPop,
+                                                                          LatinFreshmanPop, BlackFreshmanPop, AsianFreshmanPop,
+                                                                          HawaiianFreshmanPop, MixraceFreshmanPop, NoRaceFreshmanPop,
+                                                                          TotalUgradPop, TotalFreshmanPop)
+        )
+        
+        
+        output$campus_life_table = renderTable(
+                DT %>% filter(SchoolName == input$school_name) %>% select(NumFrats, NumSororities, NumStudentOrg, IntramuralSports,
+                                                                          SportsFacilities, SocailLifeComments, SocailOrganizations,
+                                                                          ReligiousOrganizations, MinorotyStudentOrganizations,
+                                                                          InternationalStudentOrganizations, OtherStudentOrganizations,
+                                                                          PercentOutstate, PercentCOEDHousing, PercentCooperativeHousing,
+                                                                          PercentDisabilityHousing, PercentWomenOnlyHousing,
+                                                                          PercentMenOnlyHousing, PercentFratHousing,
+                                                                          PercentSororityHousing, PercentSingleStudentAppartment,
+                                                                          PercentMarriedStudentAppartment, PercentInternationalStudentHousing,
+                                                                          PercentOtherHousing)
+        )
+        
+        
+        output$after_graduation_table = renderTable(
+                
+                DT %>% filter(SchoolName == input$school_name) %>% select(GradRate3Y, GradRate4Y, GradRate6Y,
+                                                                          salary_10_year, maj_rel_job_6month, maj_rel_job_12month,
+                                                                          GradSchoolList, FirmList, NotableAlumni,
+                                                                          GradMBA, GradLaw, GradMedical, GradDental, GradEngineering,
+                                                                          GradTheology, GradEducation, GradArtsSciences,
+                                                                          GradVeterinary)
+                
+        )
         
 }
--- ui.r
+++ ui.r
@@ -1,71 +1,66 @@
 library(shiny)
 library(dplyr)
+library(shinydashboard)
 
-DT = read.csv('Data/D9.csv')
-cols = c('ACTRangeUpper','SATRangeUpper','AverageAdmittedGPA','tuition_fee','rank_overall','location','scid','SchoolName')
-reco = DT[,cols]
-
-
-reco[which(reco$SATRangeUpper < 0),'SATRangeUpper'] = NA
-reco[which(reco$ACTRangeUpper == 0),'ACTRangeUpper'] = NA
-
-# add 4 columns to 'reco': volunteer_hour, award, ap_score, writing
-
-reco$volunteer_hour = 1:1878
-reco$award = 1:1878
-reco$writing = 1:1878
-reco$ap_score = 1:1878
-
-academicData = DT[,c("AverageAdmittedGPA","SATAverageComposite","SATAverageMath",
-                     "SATAverageReading","SATAverageWriting","SATRangeLower",
-                     "SATRangeUpper","ACTAverageEnglish","ACTAverageMath",
-                     "ACTAverageWriting","ACTAverageComposite",
-                     "ACTRangeLower","ACTRangeUpper",
-                     "GradLaw","GradMedical","GradDental","GradEngineering","GradTheology",
-                     "GradEducation","GradArtsSciences","GradVeterinary","GradMBA",
-                     "Specialized","SelectivityRank","SchoolName")]
-
-academicData = na.omit(academicData)
-
-academicDataMat = scale(academicData[,-25], center = TRUE, scale = TRUE)
-academicDistMat = as.matrix(dist(academicDataMat, method = "minkowski", p = 1))
-colnames(academicDistMat) = academicData$SchoolName
-row.names(academicDistMat) = academicData$SchoolName
-
-
-# manually set values
-for (i in 1:dim(reco)[1]){ 
-        if (is.na(reco$rank_overall[i])) reco[i,c('volunteer_hour','award','ap_score','writing')] = c(50,1,1,1)
-        else if (reco$rank_overall[i] <= 50)  reco[i,c('volunteer_hour','award','ap_score','writing')] = c(200,5,5,8)
-        else if (reco$rank_overall[i] <= 100)  reco[i,c('volunteer_hour','award','ap_score','writing')] = c(150,3,3,5)
-        else if (reco$rank_overall[i] <= 200)  reco[i,c('volunteer_hour','award','ap_score','writing')] = c(100,1,1,3)
-        else reco[i,c('volunteer_hour','award','ap_score','writing')] = c(50,1,1,1)
-}
-
-
-
+DT = read.csv('Data/D10.csv')
 
 
 
 shinyUI(
-        fluidPage(
-                sidebarPanel(
-                        numericInput("ACTscore", "Your ACT Score:", value = 30, min = 1, max = 36),
-                        numericInput("SATscore", "Your SAT Score:", value = 1200, min = 400, max = 1600),
-                        numericInput("GPA", "Your GPA:", value = 3.5, min = 2, max = 4),
-                        numericInput("volunteer", "Hours of Volunteer Work:", value = 20, min = 0, max = 200),
-                        numericInput("awards", "Number of Awards:", value = 2, min = 0, max = 40),
-                        numericInput("APscore", "Your AP Score:", value = 3, min = 1, max = 4),
-                        numericInput("writing", "Writing Skills:", value = 4, min = 1, max = 10),
-                        selectInput("school_name", label = "Which School do you like?", 
-                                    choices = academicData$SchoolName)
+        dashboardPage(
+                dashboardHeader(title = "CollegeRecommender"),
+                dashboardSidebar(
+                        sidebarMenu(
+                                menuItem("College Profile", tabName = "profile"),
+                                menuItem("College Chances", tabName = "chances"),
+                                menuItem("College Similarity", tabName = "similarity")
+                                )
                 ),
                 
-                mainPanel(
-                        h1("Recommended Based on Your Academics"),
-                        dataTableOutput("recommended_schools"),
-                        h1("Similar to the School you Like"),
-                        dataTableOutput("similar_school")
+                dashboardBody(
+                        tabItems(
+                                tabItem(tabName = "profile",
+                                        h1("College Profile"),
+                                        selectInput("school_name", label = "School Name", choices = unique(DT$SchoolName), 
+                                                    selected = "Boston University"),
+                                        
+                                        h2("Institution Type"),
+                                        tableOutput("institution_type_table"),
+                                        
+                                        h2("Location"),
+                                        tableOutput("location_table"),
+                                        
+                                        h2("Rankings"),
+                                        tableOutput("ranking_table"),
+                                        
+                                        h2("Admissions"),
+                                        tableOutput("admissions_table"),
+                                        
+                                        h2("Financial"),
+                                        tableOutput("financial_table"),
+                                        
+                                        h2("Academics"),
+                                        tableOutput("academics_table"),
+                                        
+                                        h2("Student Body"),
+                                        tableOutput("student_body_table"),
+                                        
+                                        h2("Campus Life"),
+                                        tableOutput("campus_life_table"),
+                                        
+                                        h2("After Graduation"),
+                                        tableOutput("after_graduation_table")
+                                        
+                                        ),
+                                tabItem(tabName = "chances",
+                                        h1("Recommended Based on Your Academics"),
+                                        tableOutput("recommended_schools")
+                                        ),
+                                tabItem(tabName = "similarity",
+                                        h1("Similar to the School you Like"),
+                                        dataTableOutput("similar_school")
+                                        )
+                        )
                 )
         )
 )
